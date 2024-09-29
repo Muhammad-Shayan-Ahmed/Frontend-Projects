a43f6cdd317105d5b55d9b94f64645bc385d6ded
Muhammad Shayan Ahmed <shayanhaider7366@gmail.com>
2024-09-29 12:17:43 +0500

    Add files via upload

---
+++ Frontend_Projects/WORKS/main.js
@@ -0,0 +1,52 @@
+function loading() {
+    var tl = gsap.timeline()
+    
+    tl.to("#yellow1", {
+            top:"-100%",
+            delay:0.5,
+            duraton:0.7,
+            ease:"expo.out"
+    })
+    
+    tl.from("#yellow2", {
+        top:"100%",
+        delay:0.5,
+        duraton:0.7,
+        ease:"expo.out"
+    }, "anim")
+    
+    tl.to("#loader h1", {
+        delay:0.6,
+        duraton:0.7,
+        color: "black"
+    }, "anim")
+    
+    tl.to("#loader", {
+        opacity: 0
+    })
+    tl.to("#loader", {
+        display: "none"
+    })
+};
+loading();
+
+function loco () {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+    
+    document.querySelector("#footer h2").addEventListener("click", () => {
+        scroll.scrollTo(0)
+    })
+    
+    var elems = document.querySelectorAll(".elem")
+    var page2 = document.querySelector("#page2")
+    elems.forEach(function(e){
+        e.addEventListener("mouseenter", function () {
+            var bgimg = e.getAttribute("data-img")
+            page2.style.backgroundImage = `url(${bgimg})`
+        })
+    })
+};
+loco();
